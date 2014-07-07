e7091ee7d77ef2522952afe132426514ad3429f4
Spaneos Mac Pc 2 <spaneosmacpc2@Spaneoss-Mac-mini.local>
2014-07-07 12:10:21 +0530

    Initial Commit

---
+++ iOSGIthub/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  iOSGIthub
+//
+//  Created by Spaneos Mac Pc 2 on 07/07/14.
+//  Copyright (c) 2014 com.spaneos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
